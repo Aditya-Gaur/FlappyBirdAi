11f82f56c2aeef63c9eb08d69fc49e8e462b68ac
Aditya Gaur <75514601+Aditya-Gaur@users.noreply.github.com>
2022-11-07 12:44:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from os import path
 import time
 import random
+import neat
 pygame.font.init()
 
 WIDTH = 400
@@ -24,17 +25,6 @@ pipe_jpg = path.join(dir_path, 'resources/pipe.png')
 retro_font = path.join(dir_path, 'resources/retro.ttf')  # absolute font path
 
 score = -1
-
-
-def move_y_axis(agent, switch=0):
-    if switch == 0: # gravity case default
-        y = agent[1]
-        y += gravity_value
-        return (agent[0], y)
-    else:           # flap
-        y = agent[1]
-        y -= flap_value
-        return (agent[0], y)
 
 class Pipe:
     def __init__(self, win, x_axis, pg):
@@ -71,12 +61,37 @@ class Pipe:
         self.rect_inverse.move_ip([-horizontal_speed, 0])
         self.show()
 
-    def manage_pipe(self):
+    def manage_pipe(self, gef=[0,[]]):
         self.move()
         if self.rect[0] <= (-PIPE_WIDTH):
             self.x_axis = WIDTH
             self.constructor()
             self.show()
+            if gef[0] != 0:
+                for g in gef[1]:
+                    g.fitness += 5
+        return gef[1]
+
+class Bird:
+    def __init__(self, win, x, y):
+        self.x =  x
+        self.y = y
+        self.rect = []
+        self.win = win
+
+    def move_y_axis(self, switch=0):
+        if switch == 0: # gravity case default
+            self.y += gravity_value
+        else:           # flap
+            self.y -= flap_value
+
+    def manage(self, ag, bg):
+        self.move_y_axis()
+#        self.rect = pygame.draw.circle(self.win, (255,255,0), (self.x, self.y), AGENT_RADIUS) # will use agent(center) to manipulate position and agent_rect to only check for collisions
+        self.rect = pygame.Rect(self.x, self.y, AGENT_RADIUS, AGENT_RADIUS)
+#        self.win.blit(bg, (0, 0)) # impliment moving img effect as x axis moves
+        self.win.blit(ag, self.rect)
+
 
 def collision_detection(agent_rect, pp1, pp2):
     if agent_rect[1] >= HEIGHT:
@@ -90,57 +105,81 @@ def collision_detection(agent_rect, pp1, pp2):
 
     return False
 
-def game_window():
+def game_window(genomes, config):
     """Game window"""
-    while True:
-        run = True
-        win = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption("Flappy bird")
-        bg = pygame.image.load(bg_jpg)
-        bg = pygame.transform.scale(bg, (WIDTH+300, HEIGHT))
-        ag = pygame.image.load(agent_jpg)
-        ag = pygame.transform.scale(ag, (AGENT_RADIUS*2.5, AGENT_RADIUS*2))
-        pg = pygame.image.load(pipe_jpg)
-        font = pygame.font.Font(retro_font, 30)
+    
+    run = True
+    win = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Flappy bird")
+    bg = pygame.image.load(bg_jpg)
+    bg = pygame.transform.scale(bg, (WIDTH+300, HEIGHT))
+    ag = pygame.image.load(agent_jpg)
+    ag = pygame.transform.scale(ag, (AGENT_RADIUS*2.5, AGENT_RADIUS*2))
+    pg = pygame.image.load(pipe_jpg)
+    font = pygame.font.Font(retro_font, 30)
+    pp = Pipe(win, (WIDTH ), pg)
+    pp2 = Pipe(win, ((WIDTH ) +  gap_btw_horizontal_pipe), pg)
+    pipe_cur = pp
 
-        agent = (WIDTH/2, HEIGHT/2) # agent center
-        user_click = False
-        pp = Pipe(win, (WIDTH ), pg)
-        pp2 = Pipe(win, ((WIDTH ) +  gap_btw_horizontal_pipe), pg)
+    agent = Bird(win, WIDTH/2, HEIGHT/2) # agent center
 
-        while run:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    user_click = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        user_click = True
-                if user_click == True:
-                    user_click = False
-                    agent = move_y_axis(agent, switch="flap")
+    agents = []
+    nets = []
+    ge = []
+    for x,g in genomes:
+        g.fitness = 0   
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        agents.append(Bird(win, WIDTH/2, HEIGHT/2))
+        ge.append(g)
 
+    while run and len(agents) > 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
             
-            agent_Rect = pygame.draw.circle(win, (255,255,0), agent, AGENT_RADIUS) # will use agent(center) to manipulate position and agent_rect to only check for collisions
-            agent = move_y_axis(agent)
+        for x,agent in enumerate(agents):
+            output = nets[x].activate((abs(agent.y), abs(agent.y - pipe_cur.rect[1]), abs(agent.y - pipe_cur.rect_inverse[1])))
+            if output[0] > 0.5:
+                agent.move_y_axis(switch="flap")
 
-            win.blit(bg, (0, 0)) # TODO impliment moving img effect as x axis moves
-            win.blit(ag, agent_Rect)
+        win.blit(bg, (0, 0)) # impliment moving img effect as x axis moves
+        for agent in agents: 
+            agent.manage(ag, bg)
 
-            label = font.render(f"Score : {score}", 1, (255,165,0))
-            win.blit(label, (0, 0))
-      
-            pp.manage_pipe()
-            pp2.manage_pipe()
-            pygame.display.update()
-            
-            if collision_detection(agent_Rect, [pp.rect, pp.rect_inverse], [pp2.rect, pp2.rect_inverse]):
-                print("You lost")
-          #      time.sleep(3)
+        label = font.render(f"Score : {score}", 1, (255,165,0))
+        win.blit(label, (0, 0))
+    
+        ge = pp.manage_pipe(gef=[1,ge])
+        ge = pp2.manage_pipe(gef=[0,ge])
+        pygame.display.update()
 
-            time.sleep(0.1)
+        pipe_cur = pp
+        if len(agents) > 0 and agents[0].x > (pipe_cur.rect[0] + pipe_cur.width):
+            pipe_cur = pp2
+
+        for x, agent in enumerate(agents):
+            if collision_detection(agent.rect, [pp.rect, pp.rect_inverse], [pp2.rect, pp2.rect_inverse]):
+                ge[x].fitness -= 1 # encourage not hittin pipes
+                agents.pop(x)
+                nets.pop(x)
+                ge.pop(x)
+
+        time.sleep(0.1)
+
+def runner(config):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config)
+    popul = neat.Population(config)
+
+    # show progress in the terminal
+    popul.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    popul.add_reporter(stats)
+    #p.add_reporter(neat.Checkpointer(5))
+
+    winner = popul.run(game_window, 10) # 50 to> 1
 
 if __name__ == '__main__':
-    game_window()
+    print("Neat")
+    config_path = path.join(dir_path, 'config.txt')
+    runner(config_path)
